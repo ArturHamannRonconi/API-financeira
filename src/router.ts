b6edfce9a4d091846da53630bb2a3f237c3fcc99
Artur Hamann Ronconi <arturhronconi@gmail.com>
2021-05-06 19:28:32 -0300

    clear routes

--- src/router.ts
+++ src/router.ts
@@ -1,32 +1,50 @@
 import { Router } from 'express'
 
 import AccountController from './controllers/AccountController'
+import AccountMiddlewares from './middlewares/AccountMiddlewares'
 
 const router = Router()
 
-const accountController = new AccountController() 
+
+const accountController = new AccountController()
+const accountMiddlewares = new AccountMiddlewares()
+
+router.use(
+  [ '/statement/:cpf', '/deposit', '/withdraw', '/balance/:cpf' ],
+  accountMiddlewares.verifyAccountExists
+)
 
 router.route('/accounts')
   .get(accountController.findAll)
   .post(
-    accountController.verifyAccountAlreadyExists,
+    accountMiddlewares.verifyIfAccountAlreadyCreated,
     accountController.create
   )
-
+  
 router.route('/accounts/:id')
   .get(accountController.findOne)
-
-
-router.route('/statement/:cpf')
-  .get(
-    accountController.verifiyExistsAccount,
-    accountController.getStatement
+  .put(
+    accountMiddlewares.verifyAccountExists,
+    accountController.update
+  )
+  .delete(
+    accountMiddlewares.verifyAccountExists,
+    accountController.delete
   )
 
+router.route('/statement/:cpf')
+  .get(accountController.getStatement)
+
+router.route('/balance/:cpf')
+  .get(accountController.getBalance)
+
 router.route('/deposit')
+  .post(accountController.deposit)
+
+router.route('/withdraw')
   .post(
-    accountController.verifiyExistsAccount,
-    accountController.deposit
+    accountMiddlewares.getBalance,
+    accountController.withdraw
   )
 
 export default router
